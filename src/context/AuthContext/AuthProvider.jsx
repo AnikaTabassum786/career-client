ed5c3193b01380e3bf06fa285c147f7b7113c724
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-06-09 12:25:29 +0600

    JWT start

--- src/context/AuthContext/AuthProvider.jsx
+++ src/context/AuthContext/AuthProvider.jsx
@@ -3,6 +3,7 @@ import { AuthContext } from './AuthContext';
 import { auth } from '../../firebase/firebase.init';
 
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut} from 'firebase/auth';
+import axios from 'axios';
 
 const AuthProvider = ({ children }) => {
 
@@ -28,6 +29,23 @@ const AuthProvider = ({ children }) => {
         const unSubscribe = onAuthStateChanged(auth,currentUser=>{
             setUser(currentUser);
             setLoading(false);
+
+            // JWT API
+
+            if(currentUser?.email){
+                const userData = {email:currentUser.email};
+                axios.post('http://localhost:3000/jwt',userData)
+                .then(res=>{
+
+                    console.log('token after JWT',res.data)
+                    console.log('Raw token:', res.data.token)
+                    
+                    const token = res.data.token
+                    localStorage.setItem('Token',token)
+                })
+                .catch(error => console.log(error))
+            }
+
             console.log('user in auth state', currentUser)
         })
         return()=>{
